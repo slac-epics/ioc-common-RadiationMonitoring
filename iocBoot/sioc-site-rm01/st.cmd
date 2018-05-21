45ec808fc9416b512f9debcdba7daf284f448790
Jacob DeFilippis <jacob.defilippis@cosylab.com>
2018-05-21 15:30:50 -0700

    activating other stations in startup cmd

--- iocBoot/sioc-site-rm01/st.cmd
+++ iocBoot/sioc-site-rm01/st.cmd
@@ -14,21 +14,25 @@ dbLoadDatabase "dbd/RadiationMonitoring.dbd"
 RadiationMonitoring_registerRecordDeviceDriver pdbbase
 
 ## Setup asyn connections PM01-PM08
-drvAsynIPPortConfigure("PM1-GAMMA","wb-site-rm01:5000")
-drvAsynIPPortConfigure("PM1-FHT6020","wb-site-rm01:5001")
+#drvAsynIPPortConfigure("PM1-GAMMA","wb-site-rm01:5000")
+drvAsynIPPortConfigure("PM1-TS","wb-site-rm01:5001")
+drvFHTConfigure("PM1-FHT6020", "PM1-TS", 1, 1.5)
+
 #
 #drvAsynIPPortConfigure("PM2-GAMMA","wb-site-rm02:5000")
 #drvAsynIPPortConfigure("PM2-FHT6020","wb-site-rm02:5001")
 #
 #drvAsynIPPortConfigure("PM3-GAMMA","wb-site-rm03:5000")
-#drvAsynIPPortConfigure("PM3-FHT6020","wb-site-rm03:5001")
+drvAsynIPPortConfigure("PM3-TS","wb-site-rm03:5001")
+drvFHTConfigure("PM3-FHT6020", "PM3-TS", 1, 1.5)
 #
 #drvAsynIPPortConfigure("PM4-GAMMA","wb-site-rm04:5000")
 #drvAsynIPPortConfigure("PM4-FHT6020","wb-site-rm04:5001")
 #
-#drvAsynIPPortConfigure("PM5-GAMMA","wb-site-rm05:5000")
-#drvAsynIPPortConfigure("PM5-FHT6020","wb-site-rm05:5001")
-#
+drvAsynIPPortConfigure("PM5-GAMMA","wb-site-rm05:5000")
+drvAsynIPPortConfigure("PM5-TS","wb-site-rm05:5001")
+drvFHTConfigure("PM5-FHT6020", "PM5-TS", 1, 1.5)
+
 #drvAsynIPPortConfigure("PM6-GAMMA","wb-site-rm06:5000")
 #drvAsynIPPortConfigure("PM6-FHT6020","wb-site-rm06:5001")
 
@@ -42,8 +46,8 @@ drvAsynIPPortConfigure("PM8-TS","wb-site-rm08:5001")
 drvFHTConfigure("PM8-FHT6020", "PM8-TS", 1, 1.5)
 
 #Prototype mobile station
-drvAsynIPPortConfigure("PM9-GAMMA","moxa-test:5004")
-drvAsynIPPortConfigure("PM9-TS","moxa-test:5003")
+drvAsynIPPortConfigure("PM9-GAMMA","moxa-test:5000")
+drvAsynIPPortConfigure("PM9-TS","moxa-test:5001")
 drvFHTConfigure("PM9-FHT6020", "PM9-TS", 1, 1.5)
 
 
@@ -56,7 +60,7 @@ dbLoadRecords("db/peripheral-station.db","UNIT=01,GPORT=PM1-GAMMA,NPORT=PM1-FHT6
 #dbLoadRecords("db/peripheral-station.db","UNIT=02,GPORT=PM2-GAMMA,NPORT=PM2-FHT6020")
 #dbLoadRecords("db/peripheral-station.db","UNIT=03,GPORT=PM3-GAMMA,NPORT=PM3-FHT6020")
 #dbLoadRecords("db/peripheral-station.db","UNIT=04,GPORT=PM4-GAMMA,NPORT=PM4-FHT6020")
-#dbLoadRecords("db/peripheral-station.db","UNIT=05,GPORT=PM5-GAMMA,NPORT=PM5-FHT6020")
+dbLoadRecords("db/peripheral-station.db","UNIT=05,GPORT=PM5-GAMMA,NPORT=PM5-FHT6020")
 #dbLoadRecords("db/peripheral-station.db","UNIT=06,GPORT=PM6-GAMMA,NPORT=PM6-FHT6020")
 dbLoadRecords("db/peripheral-station.db","UNIT=07,GPORT=PM7-GAMMA,NPORT=PM7-FHT6020")
 dbLoadRecords("db/peripheral-station.db","UNIT=08,GPORT=PM8-GAMMA,NPORT=PM8-FHT6020")
