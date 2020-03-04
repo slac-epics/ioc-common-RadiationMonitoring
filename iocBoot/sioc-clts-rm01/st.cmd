1a00d40d9b73eb57153b62540b2a04048792cf68
Michael Dunning <mdunning@slac.stanford.edu>
2020-03-04 12:10:13 -0800

    Add new IOC sioc-clts-rm01 for STP-CLTS RDM

---
+++ iocBoot/sioc-clts-rm01/st.cmd
@@ -0,0 +1,43 @@
+#!../../bin/rhel6-x86_64/RadiationMonitoring
+
+# Set environment variables
+epicsEnvSet("ENGINEER", "M. Dunning")
+epicsEnvSet("IOC_NAME", "SIOC:CLTS:RM01")
+
+# Load common piece of startup script
+< ../common/st.cmd.soft
+
+epicsEnvSet("STARTUP",  "${EPICS_SITE_TOP}/iocCommon/${IOC}")
+
+# Configure communication port
+drvAsynIPPortConfigure("RADM_CLTS_585", "ts-bsy0-pp02:2001", 0,0,0)
+
+# Initialize asyn driver
+# drvFHTConfigure(const char* port, const char* IOport, int addr, double timeout) {
+#/*------------------------------------------------------------------------------
+# * EPICS iocsh callable function to call constructor for the drvFHT class.
+# *  port    The name of the asyn port driver to be created.
+# *  IOport  The communication port name.
+# *  addr    The hardware address.
+# *  timeout The timeout for I/O (optional, default = 1.0).
+# *----------------------------------------------------------------------------*/
+drvFHTConfigure("FHT_CLTS_585", "RADM_CLTS_585", 1, 1.5)
+
+# Asyn diagnostics
+#asynSetTraceMask("RADM_CLTS_585", -1, 0x9)
+#asynSetTraceIOMask("RADM_CLTS_585", -1, 0x5)
+
+# Load record instances
+dbLoadRecords("db/sioc-clts-rm01.db", "P=RADM:CLTS:585, PORT=FHT_CLTS_585")
+
+# Configure autosave
+< iocBoot/common/init_restore.cmd.soft
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit()
+
+# Start autosave
+< ../common/start_restore.cmd.soft
+
+# End of file
+
