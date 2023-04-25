38d5242102500c9232d2ce64f08fdba46ee66522
Garth Brown <gwbrown@slac.stanford.edu>
2023-04-25 13:36:54 -0700

    Temporary EcoGamma sensor

---
+++ iocBoot/sioc-htr-rm01/st.cmd
@@ -0,0 +1,41 @@
+#!../../bin/rhel6-x86_64/RadiationMonitoring
+#==============================================================
+#
+#  Abs:  EPICS startup script for temporary EcoGamma Rad Mon device(s)
+#
+#  Name: st.cmd
+#
+#  Facility: CM01 area Radiation Monitoring Controls
+#
+#  Auth: 18-Mar-2023, Garth Brown        (gwbrown)
+#  Rev:  dd-mmm-yyyy, Reviewer's Name    (USERNAME)
+#--------------------------------------------------------------
+#  Mod:
+#
+#==============================================================
+
+# Set environment variables
+epicsEnvSet("ENGINEER",    "Garth Brown")
+epicsEnvSet("LOCATION",    "lcls-srv01")
+epicsEnvSet("IOC_NAME",    "SIOC:HTR:RM01")
+
+# Load common piece of startup script
+< ../common/st.cmd.soft
+
+drvAsynIPPortConfigure("EG1","RADM-HTR-MP01:5000")
+
+# Load record instances
+dbLoadRecords("db/asynRecord.db","P=RADM:HTR:1:,R=Asyn, PORT=EG1, ADDR=0, IMAX=0, OMAX=0")
+dbLoadRecords("db/ecogamma.template", "LOCA=HTR,UNIT=1,PORT=EG1")
+
+# Configure autosave
+< $(TOP)/iocBoot/common/init_restore.cmd.soft
+
+iocInit()
+
+# Start autosave
+< $(TOP)/iocBoot/common/start_restore.cmd.soft
+
+# End of file
+
+
