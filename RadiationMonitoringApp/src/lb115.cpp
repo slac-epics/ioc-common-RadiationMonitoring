f2efe8f80d1b15a1a69ff74e1397ca540ff9b99b
Kyle Leleux <kleleux@slac.stanford.edu>
2026-04-17 14:12:17 -0700

    Configuring class to be a singleton that runs 4 channel threads

--- RadiationMonitoringApp/src/lb115.cpp
+++ RadiationMonitoringApp/src/lb115.cpp
@@ -1,32 +1,127 @@
 #include "lb115.h"
 
+static std::mutex mtx;
+
 LB115Driver::LB115Driver(const char *portName, const char *ipPort) :
     asynPortDriver(portName,
                    1,
-                   10
+                   10,
                    asynFloat64Mask | asynInt32Mask | asynOctetMask,
                    asynFloat64Mask | asynInt32Mask | asynOctetMask,
-                   0, 1, 0, 0) {
+                   0,
+                   1,
+                   0,
+                   0) 
+{
+    
+    asynStatus driverStatus = pasynOctetSyncIO->connect(ipPort, 0, &pasynUser, NULL);
+    pasynOctetSyncIO->setInputEos(pasynUser, "\r\n", 2);
+    pasynOctetSyncIO->setOutputEos(pasynUser, "\r\n", 2);
 
-    createParam("MEAS_VAL", asynFloat64, &P_MeasVal);
-    createParam("NET_VAL", asynFloat64, &NetVal);
-    createParam("GROSS_VAL", asynFloat64, &P_GrossVal);
-    createParam("DOSE_VAL", asynFloat64, &P_DoseVal);
-    createParam("STATUS", asynInt32, &P_Status);
-    createParam("MEAS_STATUS", asynInt32, &P_MeasStatus);
-
-    pasynOctetSyncIO->connect(ipPort, 0, &pasynUser, NULL);
-
-    pollerId = epicsThreadCreate("LB115Poller",
+    if (driverStatus != asynSuccess) {
+        printf("\nFailed to connect to port %s", ipPort);
+    }
+}
+    //
+    /*pollerId = epicsThreadCreate("LB115Poller",
                                  epicsThreadPriorityMedium,
                                  epicsThreadGetStackSize(epicsThreadStackMedium),
                                  [](void *p){((LB115Driver*)p)->pollerThread();},
                                  this)
+                                 */
+
+LB115Driver& LB115Driver::getInstance(const char* portName,
+                                      const char* ipPort) {
+    
+    std::lock_guard<std::mutex> lock(mtx);
+
+    if (!_instance) {
+        _instance = new LB115Driver(portName, ipPort);
+    }
+    
+    return *_instance;
+}
+
+LB115Driver& LB115Driver::getInstance() {
+    if (!_instance) {
+        printf("LB115Driver not initialized");
+    }
+    return *_instance;
+}
+
+/*
+asynStatus LB115Driver::connect(const char *ipPort) {
+    asynStatus driverStatus = pasynOctetSyncIO->connect(ipPort, 0, &pasynUser, NULL);
+    pasynOctetSyncIO->setInputEos(pasynUser, "\r\n", 2);
+    pasynOctetSyncIO->setOutputEos(pasynUser, "\r\n", 2);
+
+    if (driverStatus != asynSuccess) {
+        printf("\nFailed to connect to port %s", ipPort);
+    }
+    return driverStatus;
+} */
+
+void LB115Driver::getData() {
+    asynStatus test;
+    size_t nBytesOut, nBytesIn;
+    int eomReason;
+    const char* buffer = "*0001900101010109**";
+    test = pasynOctetSyncIO->writeRead(pasynUser, buffer, strlen(buffer), cmdBuffer, 256, TIMEOUT, &nBytesOut, &nBytesIn, &eomReason);
+
+    printf("\n\nBytes out %ld, bytes in %ld", nBytesOut, nBytesIn);
+    printf("\n\neomReason %d", eomReason);
+    printf("\n\nError message? %s\n", pasynUser->errorMessage);
+    printf("\nBuffer:%s\n", buffer);
+    printf("\nCommand Buffer: %s\n", cmdBuffer);
+    printf("\nTest variable: %d\n", test);
+}
+
+
+extern "C" {
+    int LB115Configure(const char* portName, const char* ipPort) {
+        printf("Trying to connect to LB115 Device.");
+        try{
+            LB115Driver::getInstance(portName, ipPort);
+        }
+        catch(...) {
+            printf("Failed to configure LB115Driver.");
+        }
+    }
+    static const iocshArg LB115ConfigureArg0 = {"portName", iocshArgString};
+    static const iocshArg LB115ConfigureArg1 = {"ipPort", iocshArgString};
+    static const iocshArg * const LB115ConfigureArgs[] = {&LB115ConfigureArg0, &LB115ConfigureArg1};
+    static const iocshFuncDef LB115Configure_FuncDef = {"LB115Configure", 2, LB115ConfigureArgs};
+
+    static void LB115Configure_CallFunc(const iocshArgBuf *args){
+        LB115Configure(args[0].sval, args[1].sval);
     }
 
-void LB115Driver:sendRequest() {
-    const char *cmd = '';
-    size_t nwrite;
-    pasynOctetSyncIO->(pasynUser, cmd, strlen(cmd),TIMEOUT, &nwrite);
+    void LB115ConfigureRegister(void) {
+        iocshRegister(&LB115Configure_FuncDef, LB115Configure_CallFunc);
+    }
 
+    epicsExportRegistrar(LB115ConfigureRegister);
 }
+
+/*
+int LB115GetData() {
+    printf("Trying to get data from the LB115 Driver");
+    LB115Driver::getInstance().getData();
+}
+
+// Initializing ioc shell command arguments
+static const iocshFuncDef LB115GetData_FuncDef = {"LB115GetData", 0, NULL};
+
+// Initializing ioc shell command function
+static void LB115GetData_CallFunc(const iocshArgBuf *args) {
+    LB115GetData();
+}
+
+void LB115GetDataRegister(void) {
+    iocshRegister(&LB115GetData_FuncDef, LB115GetData_CallFunc);
+}
+
+extern "C" {
+    epicsExportRegistrar(LB115GetDataRegister);
+}
+*/
--- RadiationMonitoringApp/src/lb115.h
+++ RadiationMonitoringApp/src/lb115.h
@@ -1,59 +1,49 @@
-#include <asynPortDriver.h>
-#include <epicsThread.h>
-#include <epicsTime.h>
+#include <iostream>
+#include <cstdint>
+#include <memory>
+#include <functional>
+#include <map>
+#include <string>
+
 #include <iocsh.h>
+#include <string.h>
+#include <mutex>
+#include <atomic>
+#include <epicsExport.h>
+#include <epicsThread.h>
+#include <epicsEvent.h>
+#include <epicsTimer.h>
+#include <epicsTypes.h>
+#include <asynPortDriver.h>
 #include <asynOctetSyncIO.h>
 
-#include <string>
-#include <sstream>
-#include <map>
-#include <vector>
-#include <iostream>
-#include <cstring>
-
-#define MAX_MSG 4096
-#define TIMEOUT 1.0
+#define MAX_MSG 1024
+#define TIMEOUT 3.0
 
 class LB115Driver : public asynPortDriver {
     public:
-        LB115Driver(const char *portName,
-                    const chat *ipPort);
 
+        static LB115Driver& getInstance(const char* portName,
+                                        const char* ipPort);
+        static LB115Driver& getInstance();
+        asynStatus connect(const char* ipPort);
+        void getData();
 
     protected:
-        /*
+        
         int P_Index;
-        std::string P_DetectorDescription;
-        std::string P_DetectorDesignation;
-        int P_Status;
-        float P_MeasVal;
-        int P_MaxMeasVal;
-        std::string P_Units;
-        float P_AbsoluteUncertainty;
-        float P_RelativeUncertainty;
-        float P_Threshold; 
-        float P_DetectionLimit;
-        */
-
 
     private:
-        void pollerThread();
-        void sendRequest();
-        void readResponse();
-        void processResponse(const std::string&);
 
-        std::map<std::string, std::string> parseData(const std::string&);
-        double parseDoubleSafe(const std::string&);
-        int parseHexSafe(const std::string&);
+        LB115Driver(const char *portName,
+                    const char *ipPort);
 
-        int P_MeasVal;
-        int P_NetVal;
-        int P_GrossVal;
-        int P_DoseVal;
-        int P_Status;
-        int P_MeasStatus;
-        
+        LB115Driver(const LB115Driver&) = delete;
+        LB115Driver& operator=(const LB115Driver&) = delete;
+
+        static LB115Driver* _instance;
+
         asynUser *pasynUser;
-
-        epicsThreadId pollerId;     
-}
+        char cmdBuffer[MAX_MSG];
+        char sendBuffer[MAX_MSG];
+};
