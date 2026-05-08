c3471055391f03e517ee976fa5739bb0ac1af74f
Kyle Leleux <kleleux@slac.stanford.edu>
2026-05-08 14:32:35 -0700

    Updating dev IOC

--- iocBoot/sioc-b34-rm05/st.cmd
+++ iocBoot/sioc-b34-rm05/st.cmd
@@ -19,12 +19,12 @@
 # Set environment variables
 epicsEnvSet("ENGINEER", "K. Leleux")
 epicsEnvSet("LOCATION", "B34-253")
-epicsEnvSet("IOC_NAME", "SIOC:B34:FC273")
+epicsEnvSet("IOC_NAME", "SIOC:B34:RM253")
 #
 epicsEnvSet("PORT",         "LB115")
 epicsEnvSet("LB115_IP",     "134.79.218.86")
 epicsEnvSet("LB115_PORT",   "8123")
-#epicsEnvSet("3458A_ADDR",  "24")
+epicsEnvSet("LB115_ADDR",   "24")
 #epicsEnvSet("34970A_ADDR", "9")
 epicsEnvSet("P",           "RADM:B34:253")
 
@@ -33,23 +33,29 @@ epicsEnvSet("P",           "RADM:B34:253")
 
 # Asyn options/diagnostics
 # The 3458A needs a very long timeout when it fetches data 
-asynSetOption("$(PORT)", "$(3458A_ADDR)", "rpctimeout", 60)
+#asynSetOption("$(PORT)", "$(3458A_ADDR)", "rpctimeout", 60)
 
 #
-asynSetTraceIOMask("$(PORT)", "$(3458A_ADDR)", 0x2)
+#asynSetTraceIOMask("$(PORT)", "$(3458A_ADDR)", 0x2)
 #asynSetTraceMask("$(PORT)", 0, 0x9)
 #
-asynSetTraceIOMask("$(PORT)", "$(34970A_ADDR)", 0x2)
+#asynSetTraceIOMask("$(PORT)", "$(34970A_ADDR)", 0x2)
 #asynSetTraceMask("$(PORT)", "$(34970A_ADDR)", 0x9)
 
-drvAsynIPPortConfigure("$(PORT)", "$(LB115_IP):$(LB115_PORT)")
+drvAsynIPPortConfigure("LB115_IP", "$(LB115_IP):$(LB115_PORT)")
+lb115Configure("$(PORT)", "LB115_IP")
 
 # Load record instances
-dbLoadRecords("db/agilent_3458a.db", "P=$(P):DVM,PORT=$(PORT),ADDR=$(3458A_ADDR),PROTOFILE=agilent_3458a.proto")
-dbLoadRecords("db/asynRecord.db", "P=$(P):DVM:,R=Asyn,PORT=$(PORT),ADDR=$(3458A_ADDR),OMAX=0,IMAX=0")
+#dbLoadRecords("db/agilent_3458a.db", "P=$(P):DVM,PORT=$(PORT),ADDR=$(3458A_ADDR),PROTOFILE=agilent_3458a.proto")
+#dbLoadRecords("db/asynRecord.db", "P=$(P):DVM:,R=Asyn,PORT=$(PORT),ADDR=$(3458A_ADDR),OMAX=0,IMAX=0")
 #
-dbLoadRecords("db/agilent_34970a.db", "P=$(P):DAQ,PORT=$(PORT),ADDR=$(34970A_ADDR),PROTOFILE=agilent_34970a.proto")
-dbLoadRecords("db/asynRecord.db", "P=$(P):DAQ:,R=Asyn,PORT=$(PORT),ADDR=$(34970A_ADDR),OMAX=0,IMAX=0")
+#dbLoadRecords("db/agilent_34970a.db", "P=$(P):DAQ,PORT=$(PORT),ADDR=$(34970A_ADDR),PROTOFILE=agilent_34970a.proto")
+#dbLoadRecords("db/asynRecord.db", "P=$(P):DAQ:,R=Asyn,PORT=$(PORT),ADDR=$(34970A_ADDR),OMAX=0,IMAX=0")
+#dbLoadRecords("db/lb115_general.db", "P=$(P), PORT=$(PORT)")
+
+#dbLoadRecords("db/lb115_channels.db", "P=$(P), PORT=$(PORT), CHAN=01")
+dbLoadRecords("db/lb115.db", "P=$(P), PORT=$(PORT)")
+dbLoadRecords("db/asynRecord.db", "P=$(P),R=Asyn,PORT=LB115_IP,ADDR=$(LB115_ADDR),OMAX=0,IMAX=0")
 
 # Configure autosave
 < $(TOP)/iocBoot/common/init_restore.cmd.soft
@@ -57,7 +63,7 @@ dbLoadRecords("db/asynRecord.db", "P=$(P):DAQ:,R=Asyn,PORT=$(PORT),ADDR=$(34970A
 iocInit()
 
 # Streamdevice options
-var streamError 1
+#var streamError 1
 #var streamDebug 1
 
 # Start autosave
