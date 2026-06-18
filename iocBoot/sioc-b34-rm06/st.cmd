73493cecf7b0464dad50187c73026836eace96e4
Kyle Leleux <kleleux@slac.stanford.edu>
2026-06-18 08:55:15 -0700

    Commenting out trace mask setting.

--- iocBoot/sioc-b34-rm06/st.cmd
+++ iocBoot/sioc-b34-rm06/st.cmd
@@ -32,7 +32,7 @@ epicsEnvSet("P",           "RADM:B34:253")
 
 drvAsynIPPortConfigure("LB115_IP", "$(LB115_IP):$(LB115_PORT)")
 #asynSetTraceIOMask("LB115_IP", "$(LB115_ADDR)", 0x2)
-asynSetTraceMask("LB115_IP", "$(LB115_ADDR)", ASYN_TRACE_ERROR)
+#asynSetTraceMask("LB115_IP", "$(LB115_ADDR)", ASYN_TRACE_ERROR)
 asynSetOption("LB115_IP", 0, "disconnectOnReadTimeout", "Y")
 lb115Configure("$(PORT)", "LB115_IP", 2)
 
